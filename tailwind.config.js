b493b2c6b8b973e1b6172f1775b2784a63d21a70
irsal <irsalfa7@gmail.com>
2024-01-04 11:43:07 +0700

    Pesan komit pertama Anda di sini

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    container: {
+      center : true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily:{
+        custom: ['HIROMISAKE'],
+        nunito: ['nunito', 'sans-serif'],
+      }, 
+      borderRadius: {
+        '8xl': '2rem', 
+        '9xl': '9rem', 
+      },
+      colors:{
+        secondPurple: '#e0c0df',
+        darkPurple: '#632e7c',
+        lightPurple: '#fdfefc', 
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+     
+    },
+  },
+  plugins: [],
+}
+
