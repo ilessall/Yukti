d8db0dbfe5251c2db903c70ff2e7f6788ae3ae51
irsal <irsalfa7@gmail.com>
2024-01-08 14:24:49 +0700

    update

--- js/script.js
+++ js/script.js
@@ -20,35 +20,38 @@ hamburger.addEventListener('click', function () {
 
 
 // Slider
+let sliderContainer = document.getElementById('sliderContainer');
+let slider = document.getElementById('slider');
+let cards = slider.getElementsByTagName('li');
 
-let currentSlide = 0;
-const slides = document.querySelectorAll('.h-96 > div');
-
-function showSlide(n) {
-  slides.forEach(slide => {
-    slide.style.display = 'none';
-  });
-
-  slides[n].style.display = 'block';
+let elementsToShow = 3;
+if(document.body.clientWidth<1000){
+    elementsToShow = 1;
+}else if(document.body.clientWidth<1500){
+    elementsToShow = 2;
 }
 
-function prevSlide() {
-  if (currentSlide === 0) {
-    currentSlide = slides.length - 1;
-  } else {
-    currentSlide--;
-  }
-  showSlide(currentSlide);
+let sliderContainerWidth = sliderContainer.clientWidth;
+
+let cardWidth = sliderContainerWidth/elementsToShow;
+
+slider.style.width = cards.length*cardWidth+'px';
+slider.style.transition='margin';
+slider.style.transitionDuration='1s';
+
+
+for (let index = 0; index < cards.length; index++) {
+  const element = cards [index];
+  element.style.width = cardWidth+'px';
 }
 
-function nextSlide() {
-  if (currentSlide === slides.length - 1) {
-    currentSlide = 0;
-  } else {
-    currentSlide++;
-  }
-  showSlide(currentSlide);
+function prev(){
+
+  if (+slider.style.marginLeft.slice(0, -2) != -cardWidth * (cards.length - elementsToShow))
+       slider.style.marginLeft = ((+slider.style.marginLeft.slice (0, -2))-cardWidth)+'px';
 }
 
-showSlide(currentSlide);
-
+function next(){
+  if (+slider.style.marginLeft.slice (0, -2) != 0)
+  slider.style.marginLeft = ((+slider.style.marginLeft.slice (0, -2))+cardWidth)+'px'; // 100px
+} 
